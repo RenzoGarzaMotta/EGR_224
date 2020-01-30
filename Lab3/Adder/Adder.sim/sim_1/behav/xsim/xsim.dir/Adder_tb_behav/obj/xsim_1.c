ebfbd9f62cdabe064d6db1df0f0eaa8595ee6a0c
Renzo Garza Motta <38291108+RenzoGarzaMotta@users.noreply.github.com>
2020-01-29 21:28:43 -0500

    Fixing Git

---
+++ Lab3/Adder/Adder.sim/sim_1/behav/xsim/xsim.dir/Adder_tb_behav/obj/xsim_1.c
@@ -0,0 +1,121 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_42(char*, char *);
+extern void execute_43(char*, char *);
+extern void execute_44(char*, char *);
+extern void execute_45(char*, char *);
+extern void execute_112(char*, char *);
+extern void execute_113(char*, char *);
+extern void execute_50(char*, char *);
+extern void execute_51(char*, char *);
+extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_53(char*, char *);
+extern void execute_59(char*, char *);
+extern void execute_61(char*, char *);
+extern void execute_62(char*, char *);
+extern void vlog_const_rhs_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_76(char*, char *);
+extern void execute_107(char*, char *);
+extern void execute_108(char*, char *);
+extern void execute_109(char*, char *);
+extern void execute_47(char*, char *);
+extern void execute_48(char*, char *);
+extern void execute_49(char*, char *);
+extern void execute_114(char*, char *);
+extern void execute_115(char*, char *);
+extern void execute_116(char*, char *);
+extern void execute_117(char*, char *);
+extern void execute_118(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[27] = {(funcp)execute_42, (funcp)execute_43, (funcp)execute_44, (funcp)execute_45, (funcp)execute_112, (funcp)execute_113, (funcp)execute_50, (funcp)execute_51, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_53, (funcp)execute_59, (funcp)execute_61, (funcp)execute_62, (funcp)vlog_const_rhs_process_execute_0_fast_no_reg_no_agg, (funcp)execute_76, (funcp)execute_107, (funcp)execute_108, (funcp)execute_109, (funcp)execute_47, (funcp)execute_48, (funcp)execute_49, (funcp)execute_114, (funcp)execute_115, (funcp)execute_116, (funcp)execute_117, (funcp)execute_118, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 27;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/Adder_tb_behav/xsim.reloc",  (void **)funcTab, 27);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/Adder_tb_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/Adder_tb_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/Adder_tb_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/Adder_tb_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/Adder_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
